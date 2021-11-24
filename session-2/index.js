b50a8e77cece5920d5184c3f8597b52df08464fe
rachelgrahamtohue@gmail.com <rachelgrahamtohue@gmail.com>
2021-11-24 20:26:41 +0000

    completed task 3 on session 2

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,6 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+console.log("hello world") 
+console.log(document.querySelector("#title").innerHTML)
+console.log(document.querySelectorAll('.about'))
