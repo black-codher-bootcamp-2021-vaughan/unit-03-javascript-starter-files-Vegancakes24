388c0da08f57e9226438644392226a97678817e8
rachelgrahamtohue@gmail.com <rachelgrahamtohue@gmail.com>
2021-11-29 20:56:21 +0000

    completed 3 tasks in session 3

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,30 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5;
+const b = 10;
+
+console.log(a +b);
+
+a = 20;
+
+console.log(a+b);
+
+function sayHey() {
+    console.log("hey");
+}
+
+function conversation() {
+ sayHey();  
+ console.log("How are you?");   
+ console.log("Goodbye");
+}
+conversation();
+sayHey ();
+
+function futureAge(name, age) {
+    sayHey(name); 
+    let newAge = age + 5;
+    return newAge;
+}
+
+console.log (futureAge('Mary', 35 ));
